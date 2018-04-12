089890b12b44ea4a2885f88ed4092a9baa3587b3
Paul <paul.ryan.iii@gmail.com>
2018-04-11 17:51:04 -0700

    Solve fibonacci with dynamic programming

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -12,8 +12,8 @@ function fibonacci(num) {
   return fibs[fibs.length - 1];
 }
 
-console.log(fibonacci(12)); // 144
-console.log(fibonacci(1)); // 1
+// console.log(fibonacci(12)); // 144
+// console.log(fibonacci(1)); // 1
 
 function fibRec(num) {
   if (num <= 1) return num;
@@ -21,5 +21,25 @@ function fibRec(num) {
   return fibRec(num - 1) + fibRec(num - 2);
 }
 
-console.log(fibonacci(12)); // 144
-console.log(fibonacci(1)); // 1
+// console.log(fibonacci(12)); // 144
+// console.log(fibonacci(1)); // 1
+
+
+// can you solve it with dynamic programming?
+// We can avoid the repeated work done is the method 1 by storing the Fibonacci numbers calculated so far.
+
+// Given a number n, print n-th Fibonacci Number.
+
+function fib(num, memo) {
+  memo = memo || {};
+
+  if (memo[num]) return memo[num];
+  // this works for [1,1] as the first nums. Change to <= 1 for [0, 1]
+  if (num <= 2) return 1;
+
+  memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo);
+  return memo[num];
+}
+
+console.log(fib(12)); // 144
+console.log(fib(1)); // 1
