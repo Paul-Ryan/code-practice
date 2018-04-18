f390e2d0b0f8d4e6138df6d52cd39420d0ba8785
Paul <paul.ryan.iii@gmail.com>
2018-04-18 13:04:10 -0700

    Wrote JS heap

--- javascript/datastructures/minheap.js
+++ javascript/datastructures/minheap.js
@@ -15,13 +15,53 @@ class minHeap {
   }
 
   extract() {
+    this.size--;
 
+    // swap root and last element
+    const temp = this.heap[this.size];
+    this.heap[this.size] = this.heap[0];
+    this.heap[0] = temp;
+    const removed = this.heap.pop();
+    this.heapifyDown(0);
+
+    return removed;
   }// removes min element -
+
+  // private functions
+
+  getSmallestChildIdx(idx) {
+    const idx1 = 2 * idx + 1;
+    const idx2 = idx1 + 1;
+
+    if (this.size < idx1) return null;
+    if (this.size === idx1) return idx1;
+
+    return Math.min(idx1, idx2);
+  }
 
   getParentIdx(idx) {
     return parseInt((idx - 1) / 2 );
   }
 
+  heapifyDown(idx) {
+    if (this.size === 1) return idx;
+    const childIdx = this.getSmallestChildIdx(idx);
+    if (childIdx === null) return idx;
+
+    const current = this.heap[idx];
+    const child = this.heap[childIdx];
+
+    if (current > child) {
+      // swap current and child
+      const temp = current;
+      this.heap[idx] = child;
+      this.heap[childIdx] = temp;
+      this.heapifyDown(childIdx);
+    }
+    return idx;
+  }
+
+  // move element to correct position from bottom
   heapifyUp(idx) {
     if (this.size === 1) return idx;
 
@@ -43,7 +83,7 @@ class minHeap {
 
 const heap = new minHeap();
 console.log(heap);
-heap.insert(1);
+heap.insert(5);
 heap.insert(4);
 heap.insert(6);
 heap.insert(6);
@@ -58,4 +98,10 @@ heap.insert(9);
 heap.insert(5);
 heap.insert(11);
 heap.insert(1);
+console.log(heap);
+console.log(heap.extract());
+console.log(heap);
+console.log(heap.extract());
+console.log(heap);
+console.log(heap.extract());
 console.log(heap);
